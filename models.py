7f358cdab73a43e73ee1e394d2106b9cfc85dabe
zhoubenjia <18810387933@163.com>
2023-09-01 15:02:21 +0000

    update new

--- models.py
+++ models.py
@@ -82,7 +82,7 @@ def make_resnet(name='resnet18'):
 class resnet(nn.Module):
     def __init__(self):
         super(resnet, self).__init__()
-        self.resnet = make_resnet(name='resnet34')
+        self.resnet = make_resnet(name='resnet18')
 
     def forward(self, x, lengths):
         x = self.resnet(x)
